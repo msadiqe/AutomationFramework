63dcca08a7a387c6acd4fc312bb4a3623accf59a
sandeep.sharma <sandeepshrm96@gmail.com>
2018-09-28 10:06:23 +0530

    scr folder added to git repo

---
+++ src/config/Config.java
@@ -0,0 +1,30 @@
+package com.sephora.ePlanner.utils;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Config {
+	
+	
+	public static String path="src/test/resources/config/";
+	private static final Properties props=new Properties();
+	
+	public void loadProps(String fileName){
+		try{
+			FileInputStream input= new FileInputStream(path+fileName);
+			props.load(input);
+		}catch(FileNotFoundException file){
+			file.printStackTrace();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public String getProperty(String key){
+		return props.getProperty(key);
+	}
+
+}
